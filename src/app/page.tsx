71f399b8aecd611392bd2985efbb52fccf1af26d
momen124 <momenhossny2004@gmail.com>
2025-08-14 19:58:33 +0300

    feat: add vercel configuration and update .gitignore for lock files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import BlogStub from './components/BlogStub';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+
 export default function Home() {
   return (
     <>
